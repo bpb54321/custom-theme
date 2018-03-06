86d1106baefd878dbb4d06b6b644a822a406621b
Brian Blosser <bpb54321@gmail.com>
2018-03-06 12:18:30 -0500

    Outputting the page title and content on the front end.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
-	<h1>Hello World!</h1>
+	<h1><?php the_title(); ?></h1>
+	<?php
+		// Setup the post/page content in a global variable so that we can use other post functions
+		the_post();
+		the_content();	
+	?>
 <?php get_footer(); ?>
