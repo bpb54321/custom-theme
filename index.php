84bee6dbdd4f58da6eddba95b434d3cf45ff3b51
Brian Blosser <bpb54321@gmail.com>
2018-03-06 13:48:50 -0500

    Added a new content explaining the_post().

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-	<?php the_post(); // Setup the post/page content in a global variable so that we can use other post functions ?>
+	<?php // the_post() sets up WordPress variables so that functions like the_post() can work. ?>
+	<?php the_post(); ?>
 	<?php if ( has_post_thumbnail () ) : ?>
 		<?php the_post_thumbnail(); ?>
 	<?php endif; ?>
 	<h1><?php the_title(); ?></h1>
-	
 	<?php the_content(); ?>
 <?php get_footer(); ?>
