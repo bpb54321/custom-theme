25f3a2f256ae46ccfc5582e4a7595214a3da0c94
Brian Blosser <bpb54321@gmail.com>
2018-03-06 14:11:16 -0500

    Made there be two menu locations so the admin could select one.

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ add_theme_support( 'post-thumbnails' );
 function register_my_menus() {
   register_nav_menus(
     array(
-      'header-menu' => __( 'Header Menu' ),
+			'primary-menu' => __( 'Primary Menu' ),
+			'extra-menu' => __( 'Extra Menu' ),
      )
    );
  }
